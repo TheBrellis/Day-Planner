b63f1fcc42394e79c1683b20c344dbec078e4aea
TheBrellis <brianroyalellis@gmail.com>
2019-10-27 10:34:59 -0400

    localStorage now autopopulates text feilds. Moved localStorage script to storage.js

--- script.js
+++ script.js
@@ -5,23 +5,14 @@ const currentDay = moment().format('dddd MMMM Do')
 const currentHour = moment().format('H');
 // Setting Header to show the current date
 currentDayEl.text(currentDay);
-// Building array of objects to store textarea info into
-let allInfo = []
-for (i=9 ; i < 18; i++){
-    let thisInfo = {
-        hour: `${i}`,
-        info: ''
-    }
-    allInfo.push(thisInfo)
-}
+
 //-----------------------------------------------------------------------------------------------------------
 /* --- Modifying time-blocks --- */
 //-----------------------------------------------------------------------------------------------------------
 
-//for loop steps through every element with a data-hour attribute
+//for loop steps through every element with a data-hour attribute=
 for (i=9 ; i < 18; i++){
 let timeBlock = $(`[data-hour= ${i}]`).children().eq(1);
-
 // assigns a class to the textarea in that element depending on the current time
     if (currentHour > i) {
         timeBlock.addClass('past');
@@ -32,7 +23,7 @@ let timeBlock = $(`[data-hour= ${i}]`).children().eq(1);
     }
     //setting text content to equal allInfo array
     if (allInfo[`${i-9}`].hour == i){
-        timeBlock.text(allInfo.info);
+        timeBlock.text(allInfo[`${i-9}`].info);
     };
 };
 //-----------------------------------------------------------------------------------------------------------
